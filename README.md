48200bf276eda7be1469090b28db60e77c7dfc49
Vesa Piittinen <vesa.piittinen@gmail.com>
2024-02-06 23:44:57 +0200

    Clean it up, update things

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Merri.net
 
-Homepage. Initially based on a [Astro + SolidJS CodeSandbox template](https://github.com/Merri/codesandbox-astro-solidjs-playground).
--- src/lib/topics.ts
+++ src/lib/topics.ts
@@ -7,12 +7,6 @@ interface Topic {
 
 const topics: Topic[] = [
 	{
-		slug: 'tech',
-		title: 'Technology',
-		description: 'I follow the progress of computers and computing, and also like take look into to the past.',
-		posts: []
-	},
-	{
 		slug: 'programming',
 		title: 'Programming',
 		description: 'All sorts of coding related that may touch on things about the web, HTML, CSS, JavaScript.',
@@ -23,13 +17,6 @@ const topics: Topic[] = [
 		slug: 'modding',
 		title: 'Modding',
 		description: 'Modding games and reverse engineering is something I occasionally love to do!',
-		posts: [],
-	},
-	{
-		slug: 'autism',
-		title: 'Autism',
-		description:
-			'In 2021 I realized the INTJ "Mastermind" result I always get in personality tests means I am probably autistic.',
 		posts: [],
 	},
 ]
